88502a8b43d2f472ccc5ca136598f699d278902b
sarahlst <laouissetsarah2021@gmail.com>
2026-04-23 20:09:17 +0200

    ITER_4

--- src/ilu2/Parking.java
+++ src/ilu2/Parking.java
@@ -1,24 +1,27 @@
 package ilu2;
 
 public class Parking {
-	
+
 	private int nbPlacesLibres;
+	private int tarifHoraire;
+
 	// constructeur
-	public Parking(int nbPlaces) {
+	public Parking(int nbPlaces, int tarif) {
 		this.nbPlacesLibres = nbPlaces;
+		this.tarifHoraire = tarif;
 	}
-	
+
 	public int getNbPlacesLibres() {
 		return nbPlacesLibres;
 	}
-	
-    public void ajouter(Vehicule v) {
-    	nbPlacesLibres--;
+
+	public void ajouter(Vehicule v) {
+		nbPlacesLibres--;
 	}
-    public void retirer(Vehicule v) {
-    	nbPlacesLibres++;
-    }
-	
-	
+
+	public int retirer(Vehicule v, int nbrHeure) {
+		nbPlacesLibres++;
+		return nbrHeure * tarifHoraire;
+	}
 
 }
--- test/ilu2/ParkingTest.java
+++ test/ilu2/ParkingTest.java
@@ -9,13 +9,13 @@ class ParkingTest {
 
 	@Test
 	void test1() {
-		p = new Parking(10);
+		p = new Parking(10,0);
 		assertEquals(10,p.getNbPlacesLibres());
 	}
 	
 	@Test
 	void test2() {
-		p = new Parking(10);
+		p = new Parking(10,0);
 		Vehicule v = new Vehicule("1"); 
 		p.ajouter(v);
 		assertEquals(9,p.getNbPlacesLibres());
@@ -23,12 +23,21 @@ class ParkingTest {
 	
 	@Test
 	void test3() {
-		p = new Parking(9);
+		p = new Parking(9,0);
 		Vehicule v = new Vehicule("1"); 
-		p.retirer(v);
+		p.retirer(v,0);
 		assertEquals(10,p.getNbPlacesLibres());
 	}
 	
-	
+	@Test
+	void test4() {
+		p = new Parking(10, 3); 
+		Vehicule v = new Vehicule("1"); 
+		p.ajouter(v); 
+		p.retirer(v,5);
+		assertEquals(15,p.retirer(v,5));
+		assertEquals(11,p.getNbPlacesLibres());
+		
+	}
 
 }
