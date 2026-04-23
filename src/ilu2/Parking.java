df8e0e2fc19e9d9502216b7f5482459512c59ef9
sarahlst <laouissetsarah2021@gmail.com>
2026-04-23 21:42:45 +0200

    ITER_6

--- src/ilu2/Parking.java
+++ src/ilu2/Parking.java
@@ -4,23 +4,27 @@ public class Parking {
 
 	private int nbPlacesLibres;
 	private int tarifHoraire;
+	private Vehicule[] vehiculeGare;
+	private int nbrVehiculeGaree = 0;
 
 	// constructeur
 	public Parking(int nbPlaces, int tarif) {
 		this.nbPlacesLibres = nbPlaces;
 		this.tarifHoraire = tarif;
+        this.vehiculeGare = new Vehicule[nbPlaces];
 	}
 
 	public int getNbPlacesLibres() {
 		return nbPlacesLibres;
 	}
-
+    
 	public boolean ajouter(Vehicule v) {
 		if ( nbPlacesLibres == 0 )
 		{
 			return false;
 		}
 		else {
+			vehiculeGare[nbrVehiculeGaree]=v;
 			nbPlacesLibres--;
 			return true;
 		}
@@ -30,6 +34,18 @@ public class Parking {
 	public int retirer(Vehicule v, int nbrHeure) {
 		nbPlacesLibres++;
 		return nbrHeure * tarifHoraire;
+	}
+	
+	public boolean contient(Vehicule v) {
+		
+		for ( Vehicule v1 : vehiculeGare )
+		{
+			if ( v.equals(v1) ) {
+				return true;
+			}
+		}
+		return false;
+		
 	}
 	
 
--- test/ilu2/ParkingTest.java
+++ test/ilu2/ParkingTest.java
@@ -9,46 +9,59 @@ class ParkingTest {
 
 	@Test
 	void test1() {
-		p = new Parking(10,0);
-		assertEquals(10,p.getNbPlacesLibres());
+		p = new Parking(10, 0);
+		assertEquals(10, p.getNbPlacesLibres());
 	}
-	
+
 	@Test
 	void test2() {
-		p = new Parking(10,0);
-		Vehicule v = new Vehicule("1"); 
+		p = new Parking(10, 0);
+		Vehicule v = new Vehicule("1");
 		p.ajouter(v);
-		assertEquals(9,p.getNbPlacesLibres());
+		assertEquals(9, p.getNbPlacesLibres());
 	}
-	
+
 	@Test
 	void test3() {
-		p = new Parking(9,0);
-		Vehicule v = new Vehicule("1"); 
-		p.retirer(v,0);
-		assertEquals(10,p.getNbPlacesLibres());
+		p = new Parking(9, 0);
+		Vehicule v = new Vehicule("1");
+		p.retirer(v, 0);
+		assertEquals(10, p.getNbPlacesLibres());
 	}
-	
+
 	@Test
 	void test4() {
-		p = new Parking(10, 3); 
-		Vehicule v = new Vehicule("1"); 
-		p.ajouter(v); 
-		p.retirer(v,5);
-		assertEquals(15,p.retirer(v,5));
-		assertEquals(11,p.getNbPlacesLibres());
-		
-	}
-	@Test 
-	void test5() {
-		p = new Parking(10, 3); 
-		Vehicule v = new Vehicule("1"); 
+		p = new Parking(10, 3);
+		Vehicule v = new Vehicule("1");
 		p.ajouter(v);
-		assertEquals(true,p.ajouter(v));
-		Parking p1 = new Parking(0, 3); 
-		Vehicule v1 = new Vehicule("2"); 
+		p.retirer(v, 5);
+		assertEquals(15, p.retirer(v, 5));
+		assertEquals(11, p.getNbPlacesLibres());
+
+	}
+
+	@Test
+	void test5() {
+		p = new Parking(10, 3);
+		Vehicule v = new Vehicule("1");
+		p.ajouter(v);
+		assertEquals(true, p.ajouter(v));
+		Parking p1 = new Parking(0, 3);
+		Vehicule v1 = new Vehicule("2");
 		p.ajouter(v1);
-	    assertEquals(false,p1.ajouter(v));
+		assertEquals(false, p1.ajouter(v));
+	}
+
+	@Test
+	void test6() {
+		Parking p = new Parking(10, 3);  
+		Vehicule v = new Vehicule("1"); 
+		Vehicule v1 = new Vehicule("2");
+		p.ajouter(v); 
+		assertEquals(true,p.contient(v));
+		assertEquals(false, p.contient(v1));
+		
+		
 	}
 
 }
